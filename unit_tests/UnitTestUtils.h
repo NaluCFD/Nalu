821f183bc291c731e4c9dfe3d68376a04bd00dfc
Robert C. Knaus <rcknaus@sandia.gov>
2016-10-28 16:38:30 -0600

    Start adding some master element unit tests

--- unit_tests/UnitTestUtils.h
+++ unit_tests/UnitTestUtils.h
@@ -4,7 +4,9 @@
 namespace unit_test_utils {
 
 void fill_mesh_1_elem_per_proc_hex8(stk::mesh::BulkData& bulk);
-void create_one_hex27_element(stk::mesh::BulkData& bulk);
+
+void create_one_reference_hex8_element(stk::mesh::BulkData& bulk);
+void create_one_reference_hex27_element(stk::mesh::BulkData& bulk);
 
 }
 
