bb426244372a9c831d296a4b285886810a25c375
Stefan P. Domino <nalucfd@gmail.com>
2017-02-09 08:09:37 -0700

    Revert "Add a supplemental algorithm builder function"

--- include/AlgTraits.h
+++ include/AlgTraits.h
@@ -9,8 +9,6 @@
 #ifndef AlgTraits_h
 #define AlgTraits_h
 
-#include <stk_topology/topology.hpp>
-
 namespace sierra {
 namespace nalu {
 
@@ -21,7 +19,6 @@ struct AlgTraitsHex8 {
   static constexpr int numScsIp_ = 12;
   static constexpr int numScvIp_ = 8;
   static constexpr int numGp_ = 8; // for FEM
-  static constexpr stk::topology::topology_t topo_ = stk::topology::HEX_8;
 };
 
 struct AlgTraitsTet4 {
@@ -30,7 +27,6 @@ struct AlgTraitsTet4 {
   static constexpr int numScsIp_ = 6;
   static constexpr int numScvIp_ = 4;
   static constexpr int numGp_ = 4; // for FEM
-  static constexpr stk::topology::topology_t topo_ = stk::topology::TET_4;
 };
 
 struct AlgTraitsPyr5 {
@@ -39,7 +35,6 @@ struct AlgTraitsPyr5 {
   static constexpr int numScsIp_ = 8;
   static constexpr int numScvIp_ = 5;
   static constexpr int numGp_ = 5; // for FEM
-  static constexpr stk::topology::topology_t topo_ = stk::topology::PYRAMID_5;
 };
 
 struct AlgTraitsWed6 {
@@ -48,7 +43,6 @@ struct AlgTraitsWed6 {
   static constexpr int numScsIp_ = 9;
   static constexpr int numScvIp_ = 6;
   static constexpr int numGp_ = 6; // for FEM
-  static constexpr stk::topology::topology_t topo_ = stk::topology::WEDGE_6;
 };
 
 } // namespace nalu
--- include/HeatCondEquationSystem.h
+++ include/HeatCondEquationSystem.h
@@ -82,11 +82,7 @@ public:
     EquationSystem::load(node);
     get_if_present(node, "use_collocation", collocationForViscousTerms_, false);
   }
-
-  void report_invalid_supp_alg_names();
-  void report_built_supp_alg_names();
-
-
+  
   // allow equation system to manage a projected nodal gradient
   const bool managePNG_;
 
@@ -109,8 +105,6 @@ public:
   bool isInit_;
   bool collocationForViscousTerms_;
   ProjectedNodalGradientEquationSystem *projectedNodalGradEqs_;
-
-  const std::string suppAlgTypeName_;
 };
 
 } // namespace nalu
