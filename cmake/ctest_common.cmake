ec44f42f791eee5e94e8fbde7357ef33803eb6cb
Rood <jon.rood@nrel.gov>
2017-02-22 16:58:43 -0700

    Updating ctest code to help in testing on the Mac.

--- cmake/ctest_common.cmake
+++ cmake/ctest_common.cmake
@@ -8,21 +8,21 @@ else(NOT ${NIGHTLY_DIR} STREQUAL "")
   message( FATAL_ERROR "You need to set the NIGHTLY_DIR variable. CMake will exit." )
 endif(NOT ${NIGHTLY_DIR} STREQUAL "")
 
-set(NALU_DIR                         "${NIGHTLY_DIR}/Nalu")
-set(NALURTEST_DIR                    "${NIGHTLY_DIR}/NaluRtest")
-set(NALURTEST_MESH_DIR               "${NALURTEST_DIR}/mesh")
-set(NALURTEST_XML_DIR                "${NALURTEST_DIR}/xml")
-set(NALURTEST_NIGHTLY_DIR            "${NALURTEST_DIR}/nightly")
-set(NALURTEST_PERF_DIR               "${NALURTEST_DIR}/performance")
-set(TEST_RESULT_DIRECTORY            "${NIGHTLY_DIR}/runNaluRtest")
-set(NIGHTLY_TEST_RESULT_DIRECTORY    "${TEST_RESULT_DIRECTORY}/nightly")
-set(PERF_TEST_RESULT_DIRECTORY       "${TEST_RESULT_DIRECTORY}/performance")
+set(NALU_DIR                        "${NIGHTLY_DIR}/Nalu")
+set(NALURTEST_DIR                   "${NIGHTLY_DIR}/NaluRtest")
+set(NALURTEST_MESH_DIR              "${NALURTEST_DIR}/mesh")
+set(NALURTEST_XML_DIR               "${NALURTEST_DIR}/xml")
+set(NALURTEST_NIGHTLY_DIR           "${NALURTEST_DIR}/nightly")
+set(NALURTEST_PERF_DIR              "${NALURTEST_DIR}/performance")
+set(TEST_RESULT_DIRECTORY           "${NIGHTLY_DIR}/runNaluRtest")
+set(NIGHTLY_TEST_RESULT_DIRECTORY   "${TEST_RESULT_DIRECTORY}/nightly")
+set(PERF_TEST_RESULT_DIRECTORY      "${TEST_RESULT_DIRECTORY}/performance")
 
 # -----------------------------------------------------------
 # -- REPOS
 # -----------------------------------------------------------
-set(NALU_REPO_URL                    "https://github.com/NaluCFD/Nalu.git")
-set(NALURTEST_REPO_URL               "https://github.com/NaluCFD/NaluRtest.git")
+set(NALU_REPO_URL                   "https://github.com/NaluCFD/Nalu.git")
+set(NALURTEST_REPO_URL              "https://github.com/NaluCFD/NaluRtest.git")
 
 # -----------------------------------------------------------
 # -- Get environment
@@ -31,9 +31,14 @@ set(NALURTEST_REPO_URL               "https://github.com/NaluCFD/NaluRtest.git")
 ## -- Set hostname
 ## --------------------------
 find_program(HOSTNAME_CMD NAMES hostname)
-exec_program("${HOSTNAME_CMD} -d" ARGS OUTPUT_VARIABLE HOSTNAME)
-
-set(CTEST_SITE                          "${HOSTNAME}")
+if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+  exec_program("${HOSTNAME_CMD} -f" ARGS OUTPUT_VARIABLE HOSTNAME)
+elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+  exec_program("${HOSTNAME_CMD} -d" ARGS OUTPUT_VARIABLE HOSTNAME)
+endif()
+if(${HOSTNAME} MATCHES "hpc.nrel.gov")
+  set(HOSTNAME "peregrine.hpc.nrel.gov")
+endif()
 
 ## -- Set site / build name
 ## --------------------------
@@ -41,12 +46,10 @@ find_program(UNAME NAMES uname)
 macro(getuname name flag)
   exec_program("${UNAME}" ARGS "${flag}" OUTPUT_VARIABLE "${name}")
 endmacro(getuname)
-  
-getuname(osname -s)
-getuname(osrel  -r)
-getuname(cpu    -m)
+getuname(CPU -m)
 
-set(CTEST_BUILD_NAME                    "${osname}-${cpu}-${COMPILER_NAME}-${TRILINOS_BRANCH}")
+set(CTEST_SITE "${HOSTNAME}")
+set(CTEST_BUILD_NAME "${CMAKE_SYSTEM_NAME}-${CPU}-${COMPILER_NAME}-${TRILINOS_BRANCH}")
 
 ## -- Git command
 ## ----------------
@@ -60,25 +63,25 @@ find_program(MAKE NAMES make)
 # -- build specific
 # -----------------------------------------------------------
 
-set(MODEL                               "nightly")
+set(MODEL                           "nightly")
 
 ## -- SRC Dir
-set(CTEST_SOURCE_DIRECTORY              "${NALU_DIR}")
+set(CTEST_SOURCE_DIRECTORY          "${NALU_DIR}")
 
 ## -- BIN Dir
-set(CTEST_BINARY_DIRECTORY              "${NALU_DIR}/build")
+set(CTEST_BINARY_DIRECTORY          "${NALU_DIR}/build")
 
 ## -- Binary names
-set(CTEST_NALU_BINARY_NAME              "${CTEST_BINARY_DIRECTORY}/naluX")
-set(CTEST_UNITTEST_BINARY_NAME          "${CTEST_BINARY_DIRECTORY}/unittestX")
+set(CTEST_NALU_BINARY_NAME          "${CTEST_BINARY_DIRECTORY}/naluX")
+set(CTEST_UNITTEST_BINARY_NAME      "${CTEST_BINARY_DIRECTORY}/unittestX")
 
 ## -- Build options 
 include(ProcessorCount)
 ProcessorCount(NP)
 message(STATUS "Number of processors detected: ${NP}")
-set(CTEST_BUILD_FLAGS                   -j${NP})
-set(CTEST_PARALLEL_LEVEL                ${NP})
-set(OPTION_BUILD                        "-j${NP}")
+set(CTEST_BUILD_FLAGS               -j${NP})
+set(CTEST_PARALLEL_LEVEL            ${NP})
+set(OPTION_BUILD                    "-j${NP}")
 
 # -----------------------------------------------------------
 # -- commands
@@ -86,17 +89,17 @@ set(OPTION_BUILD                        "-j${NP}")
 
 ## -- Checkout command
 if(NOT EXISTS "${CTEST_SOURCE_DIRECTORY}")
-        set(CTEST_CHECKOUT_COMMAND     "${CTEST_GIT_COMMAND} clone ${NALU_REPO_URL} ${CTEST_SOURCE_DIRECTORY}")
+        set(CTEST_CHECKOUT_COMMAND "${CTEST_GIT_COMMAND} clone ${NALU_REPO_URL} ${CTEST_SOURCE_DIRECTORY}")
 endif(NOT EXISTS "${CTEST_SOURCE_DIRECTORY}")
        
 ## -- Update Command
-set(CTEST_UPDATE_COMMAND               "${CTEST_GIT_COMMAND}")
+set(CTEST_UPDATE_COMMAND "${CTEST_GIT_COMMAND}")
 
 ## -- Configure Command
-set(CTEST_CONFIGURE_COMMAND            "cmake -DTrilinos_DIR:PATH=${TRILINOS_DIR} -DYAML_DIR:PATH=${YAML_DIR} -DENABLE_INSTALL:BOOL=OFF -DCMAKE_BUILD_TYPE=RELEASE ${CTEST_SOURCE_DIRECTORY}")
+set(CTEST_CONFIGURE_COMMAND "cmake -DTrilinos_DIR:PATH=${TRILINOS_DIR} -DYAML_DIR:PATH=${YAML_DIR} -DENABLE_INSTALL:BOOL=OFF -DCMAKE_BUILD_TYPE=RELEASE ${CTEST_SOURCE_DIRECTORY}")
 
 ## -- Build Command
-set(CTEST_BUILD_COMMAND                "${MAKE} ${OPTION_BUILD}")
+set(CTEST_BUILD_COMMAND "${MAKE} ${OPTION_BUILD}")
 
 # -----------------------------------------------------------
 # -- Configure CTest
