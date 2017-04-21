f89b22383b491c856237be6b4ef29c340d5356c8
Jon Rood <jon.rood@nrel.gov>
2017-04-21 14:46:43 -0600

    Simplifying CTest nightly script.

--- reg_tests/CTestNightlyScript.cmake
+++ reg_tests/CTestNightlyScript.cmake
@@ -15,12 +15,12 @@ endif()
 # -----------------------------------------------------------
 
 # Set important configuration variables
-set(MODEL "nightly")
-set(NALU_DIR  "${NIGHTLY_DIR}/Nalu")
+set(NALU_DIR "${NIGHTLY_DIR}/Nalu")
 set(CTEST_SITE "${HOST_NAME}")
 set(CTEST_BUILD_NAME "${CMAKE_SYSTEM_NAME}${EXTRA_BUILD_NAME}")
 set(CTEST_SOURCE_DIRECTORY "${NALU_DIR}")
 set(CTEST_BINARY_DIRECTORY "${NALU_DIR}/build")
+set(CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
 find_program(CTEST_GIT_COMMAND NAMES git)
 find_program(MAKE NAMES make)
 
@@ -28,46 +28,45 @@ find_program(MAKE NAMES make)
 include(ProcessorCount)
 ProcessorCount(NP)
 message(STATUS "Number of processors detected: ${NP}")
-set(CTEST_BUILD_FLAGS -j${NP})
+set(CTEST_BUILD_FLAGS "-j${NP}")
 set(CTEST_PARALLEL_LEVEL ${NP})
-set(MAKE_OPTIONS "-j${NP}")
 
 # Update Command
 set(CTEST_UPDATE_COMMAND "${CTEST_GIT_COMMAND}")
 set(CTEST_GIT_INIT_SUBMODULES "ON")
 
 # Configure Command
-set(CTEST_CONFIGURE_COMMAND "cmake -DTrilinos_DIR:PATH=${TRILINOS_DIR} -DYAML_DIR:PATH=${YAML_DIR} -DENABLE_INSTALL:BOOL=OFF -DCMAKE_BUILD_TYPE=RELEASE -DENABLE_TESTS=ON ${CTEST_SOURCE_DIRECTORY}")
+set(CTEST_CONFIGURE_COMMAND "cmake -DTrilinos_DIR:PATH=${TRILINOS_DIR} -DYAML_DIR:PATH=${YAML_DIR} -DENABLE_TESTS=ON ${CTEST_SOURCE_DIRECTORY}")
 
 # Build Command
-set(CTEST_BUILD_COMMAND "${MAKE} ${MAKE_OPTIONS}")
+set(CTEST_BUILD_COMMAND "${MAKE} ${CTEST_BUILD_FLAGS}")
 
 # -----------------------------------------------------------
 # -- Run CTest
 # -----------------------------------------------------------
 
-ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+#ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
  
-message(" -- Start dashboard ${MODEL} - ${CTEST_BUILD_NAME} --")
-ctest_start(${MODEL} TRACK ${MODEL})
+message(" -- Start dashboard - ${CTEST_BUILD_NAME} --")
+ctest_start("Nightly" TRACK "Nightly")
 
-message(" -- Update ${MODEL} - ${CTEST_BUILD_NAME} --")
+message(" -- Update - ${CTEST_BUILD_NAME} --")
 ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}" RETURN_VALUE res)
 
-message(" -- Configure ${MODEL} - ${CTEST_BUILD_NAME} --")
+message(" -- Configure - ${CTEST_BUILD_NAME} --")
 ctest_configure(BUILD "${CTEST_BINARY_DIRECTORY}" RETURN_VALUE res)
 
-message(" -- Build ${MODEL} - ${CTEST_BUILD_NAME} --")
+message(" -- Build - ${CTEST_BUILD_NAME} --")
 ctest_build(BUILD "${CTEST_BINARY_DIRECTORY}" RETURN_VALUE res)
 
-message(" -- Test ${MODEL} - ${CTEST_BUILD_NAME} --")
+message(" -- Test - ${CTEST_BUILD_NAME} --")
 ctest_test(BUILD "${CTEST_BINARY_DIRECTORY}"
            PARALLEL_LEVEL ${CTEST_PARALLEL_LEVEL}
            RETURN_VALUE res)
 
-message(" -- Submit ${MODEL} - ${CTEST_BUILD_NAME} --")
+message(" -- Submit - ${CTEST_BUILD_NAME} --")
 ctest_submit(RETRY_COUNT 20
              RETRY_DELAY 20
              RETURN_VALUE res)
 
-message(" -- Finished ${MODEL} - ${CTEST_BUILD_NAME} --")
+message(" -- Finished - ${CTEST_BUILD_NAME} --")
