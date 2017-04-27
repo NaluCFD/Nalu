d6043af671a08c3151a549590ca2bca46810acd1
Shreyas Ananthan <shreyas.ananthan@nrel.gov>
2017-04-27 18:26:58 -0400

    Add unit test for MomentumAdvDiffElemKernel

--- unit_tests/kernels/UnitTestKernelUtils.C
+++ unit_tests/kernels/UnitTestKernelUtils.C
@@ -165,6 +165,89 @@ void temperature_test_function(
   init_trigonometric_field(bulk, coordinates, temperature);
 }
 
+
+void calc_mass_flow_rate_scs(
+  const stk::mesh::BulkData& bulk,
+  const stk::topology& topo,
+  const VectorFieldType& coordinates,
+  const ScalarFieldType& density,
+  const VectorFieldType& velocity,
+  const GenericFieldType& massFlowRate)
+{
+  const auto& meta = bulk.mesh_meta_data();
+  const int ndim = meta.spatial_dimension();
+  EXPECT_EQ(ndim, 3);
+
+  sierra::nalu::ElemDataRequests dataNeeded;
+
+  const ScalarFieldType& densityNp1 = density.field_of_state(stk::mesh::StateNP1);
+  const VectorFieldType& velocityNp1 = velocity.field_of_state(stk::mesh::StateNP1);
+  auto meSCS = sierra::nalu::get_surface_master_element(topo);
+
+  dataNeeded.add_cvfem_surface_me(meSCS);
+  dataNeeded.add_gathered_nodal_field(coordinates, ndim);
+  dataNeeded.add_gathered_nodal_field(densityNp1, 1);
+  dataNeeded.add_gathered_nodal_field(velocityNp1, ndim);
+  dataNeeded.add_master_element_call(sierra::nalu::SCS_AREAV);
+
+  const stk::mesh::Selector selector =
+    meta.locally_owned_part() | meta.globally_shared_part();
+  const auto& buckets = bulk.get_buckets(stk::topology::ELEM_RANK, selector);
+
+  const int bytes_per_team = 0;
+  const int bytes_per_thread = sierra::nalu::get_num_bytes_pre_req_data(
+    dataNeeded, meta.spatial_dimension()) ;
+
+  auto team_exec = sierra::nalu::get_team_policy(
+    buckets.size(), bytes_per_team, bytes_per_thread);
+
+  Kokkos::parallel_for(team_exec, [&](const sierra::nalu::TeamHandleType& team) {
+      auto& b = *buckets[team.league_rank()];
+      const auto length = b.size();
+
+      EXPECT_EQ(b.topology(), topo);
+
+      sierra::nalu::ScratchViews preReqData(
+        team, bulk, topo, dataNeeded);
+
+      Kokkos::parallel_for(
+        Kokkos::TeamThreadRange(team, length), [&](const size_t& k) {
+          stk::mesh::Entity element = b[k];
+          sierra::nalu::fill_pre_req_data(
+            dataNeeded, bulk, topo, element, &coordinates, preReqData);
+
+          std::vector<double> rhoU(ndim);
+          std::vector<double> v_shape_function(
+            meSCS->numIntPoints_ * meSCS->nodesPerElement_);
+          meSCS->shape_fcn(v_shape_function.data());
+          double *mdot = stk::mesh::field_data(massFlowRate, element);
+          auto& v_densityNp1 = preReqData.get_scratch_view_1D(densityNp1);
+          auto& v_velocityNp1 = preReqData.get_scratch_view_2D(velocityNp1);
+          auto& v_scs_areav = preReqData.scs_areav;
+
+          for (int ip=0; ip < meSCS->numIntPoints_; ++ip) {
+            for (int j=0; j < ndim; ++j) {
+              rhoU[j] = 0.0;
+            }
+            const int offset = ip * meSCS->nodesPerElement_;
+
+            for (int ic=0; ic < meSCS->nodesPerElement_; ++ic) {
+              const double r = v_shape_function[offset+ic];
+              for (int j=0; j < ndim; ++j) {
+                rhoU[j] += r * v_densityNp1(ic) * v_velocityNp1(ic, j);
+              }
+            }
+
+            double tmdot = 0.0;
+            for (int j=0; j < ndim; j++) {
+              tmdot += rhoU[j] * v_scs_areav(ip, j);
+            }
+            mdot[ip] = tmdot;
+          }
+        });
+    });
+}
+
 void expect_all_near(
   const sierra::nalu::SharedMemView<double*>& calcValue,
   const double* exactValue,
