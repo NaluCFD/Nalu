886f90a2e4d75de66cef7d4bc0663092afea9d32
Stefan P. Domino <spdomin@sandia.gov>
2017-11-29 13:32:28 -0700

    Add missing header for last NSO
    Notes:
    
    a) My apologies. Not sure how I missed that one..

---
+++ include/nso/ScalarNSOKeElemKernel.h
@@ -0,0 +1,76 @@
+/*------------------------------------------------------------------------*/
+/*  Copyright 2014 National Renewable Energy Laboratory.                  */
+/*  This software is released under the license detailed                  */
+/*  in the file, LICENSE, which is located in the top-level Nalu          */
+/*  directory structure                                                   */
+/*------------------------------------------------------------------------*/
+
+#ifndef SCALARNSOKEELEMKERNEL_H
+#define SCALARNSOKEELEMKERNEL_H
+
+#include "Kernel.h"
+#include "FieldTypeDef.h"
+
+#include <stk_mesh/base/BulkData.hpp>
+#include <stk_mesh/base/Entity.hpp>
+
+#include <Kokkos_Core.hpp>
+
+namespace sierra {
+namespace nalu {
+
+class SolutionOptions;
+class MasterElement;
+class ElemDataRequests;
+
+/** NSO ke for scalar
+ *
+ */
+template<typename AlgTraits>
+class ScalarNSOKeElemKernel : public Kernel
+{
+public:
+  ScalarNSOKeElemKernel(
+    const stk::mesh::BulkData&,
+    const SolutionOptions& solnOpts,
+    ScalarFieldType* scalarQ,
+    VectorFieldType* Gjq,
+    const double turbCoeff,
+    const double fourthFac,
+    ElemDataRequests& dataPreReqs);
+
+  virtual ~ScalarNSOKeElemKernel() {}
+
+  virtual void execute(
+    SharedMemView<DoubleType**>&,
+    SharedMemView<DoubleType*>&,
+    ScratchViews<DoubleType>&);
+
+private:
+  ScalarNSOKeElemKernel() = delete;
+
+  ScalarFieldType *scalarQ_{nullptr};
+  VectorFieldType *Gjq_{nullptr};
+  VectorFieldType *coordinates_{nullptr};
+  VectorFieldType *velocityNp1_{nullptr};
+  VectorFieldType *velocityRTM_{nullptr};
+  VectorFieldType *Gjp_{nullptr};
+  ScalarFieldType *densityNp1_{nullptr};
+  ScalarFieldType *pressure_{nullptr};
+
+  // master element
+  const int *lrscv_;
+  const double fourthFac_;
+  const double turbCoeff_;
+  const double Cupw_{0.1};
+  const double small_{1.0e-16};
+  const bool shiftedGradOp_;
+
+  // fixed scratch space
+  Kokkos::View<DoubleType[AlgTraits::numScsIp_][AlgTraits::nodesPerElement_]> v_shape_function_{"v_shape_function"};
+};
+
+}  // nalu
+}  // sierra
+
+#endif /* SCALARNSOKEELEMKERNEL_H */
