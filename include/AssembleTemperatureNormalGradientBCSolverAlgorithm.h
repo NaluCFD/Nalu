d94a4680da72ede099e0d6ebc6e9234c11c1185c
Matt Churchfield <matt.churchfield@nrel.gov>
2017-12-04 15:02:51 -0700

    F/grad bc (#259)
    * Progress toward symmetry BC with temperature gradient
    
    * Progress toward symmetry BC with temperature gradient
    
    * Adding ability to specify a boundary normal gradient of temperature through a compatible flux.
    
    * Updates to the ablForcingEdge regression test
    
    * Updating NaluMesh submodule with new ABL mesh
    
    * Removing commented print out line from AssembleScalarNormalGradientBCSolverAlgorithm
    
    * Changed name from AssembleScalarNormal... to AssembleTemperatureNormal...

---
+++ include/AssembleTemperatureNormalGradientBCSolverAlgorithm.h
@@ -0,0 +1,56 @@
+/*------------------------------------------------------------------------*/
+/*  Copyright 2014 Sandia Corporation.                                    */
+/*  This software is released under the license detailed                  */
+/*  in the file, LICENSE, which is located in the top-level Nalu          */
+/*  directory structure                                                   */
+/*------------------------------------------------------------------------*/
+
+#ifndef AssembleTemperatureNormalGradientBCSolverAlgorithm_h
+#define AssembleTemperatureNormalGradientBCSolverAlgorithm_h
+
+#include<SolverAlgorithm.h>
+#include<FieldTypeDef.h>
+
+namespace stk {
+namespace mesh {
+class Part;
+}
+}
+
+namespace sierra{
+namespace nalu{
+
+class LinearSystem;
+class Realm;
+
+class AssembleTemperatureNormalGradientBCSolverAlgorithm : public SolverAlgorithm
+{
+public:
+
+  AssembleTemperatureNormalGradientBCSolverAlgorithm(
+    Realm &realm,
+    stk::mesh::Part *part,
+    EquationSystem *eqSystem,
+    ScalarFieldType *bcScalarGrad,
+    ScalarFieldType *evisc,
+    ScalarFieldType *specHeat,
+    bool useShifted);
+  virtual ~AssembleTemperatureNormalGradientBCSolverAlgorithm() {}
+  virtual void initialize_connectivity();
+  virtual void execute();
+
+private:
+
+  const bool useShifted_;
+
+  ScalarFieldType *bcScalarGrad_;
+  ScalarFieldType *evisc_;
+  ScalarFieldType *specHeat_;
+  GenericFieldType *exposedAreaVec_;
+};
+
+}
+}
+
+
+#endif /* ASSEMBLESCALARELEMDIFFBCSOLVERALGORITHM_H_ */
