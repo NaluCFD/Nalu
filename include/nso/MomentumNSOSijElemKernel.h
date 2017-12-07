f285d29e021b4cadd6f80e24c6e7dbb1d1032fb1
Nalu It <naluIt@Nalus-Mac-Pro.local>
2017-12-07 05:03:10 -0700

    MomentumNSPSijElemKernel; copy paste

--- include/nso/MomentumNSOSijElemKernel.h
+++ include/nso/MomentumNSOSijElemKernel.h
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------------*/
-/*  Copyright 2014 National Renewable Energy Laboratory.                  */
+/*  Copyright 2014 Sandia Corporation.                                    */
 /*  This software is released under the license detailed                  */
 /*  in the file, LICENSE, which is located in the top-level Nalu          */
 /*  directory structure                                                   */
