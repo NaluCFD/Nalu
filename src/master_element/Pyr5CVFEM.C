b7b28ad518aac93b09ad0925378a4e231fb7df09
Stefan P. Domino <nalucfd@gmail.com>
2018-06-14 15:09:31 -0600

    Allow for shifted at all pyr5 faces as we use a different basis. (#397)

--- src/master_element/Pyr5CVFEM.C
+++ src/master_element/Pyr5CVFEM.C
@@ -1025,15 +1025,13 @@ void PyrSCS::shifted_face_grad_op(
   // ordinal four is a quad4
   const int npf = (face_ordinal < 4 ) ? 3 : 4;
 
-  // quad4 is the only face that can be safely shifted
-  const double *p_intgExp = (face_ordinal < 4 ) ? &intgExpFace_[0] : &intgExpFaceShift_[0];
   for ( int n=0; n<nelem; n++ ) {
 
     for ( int k=0; k<npf; k++ ) {
 
       const int row = 9*face_ordinal + k*ndim;
-      shifted_pyr_derivative(nface, &p_intgExp[row], dpsi);
-
+      shifted_pyr_derivative(nface, &intgExpFaceShift_[row], dpsi);
+      
       SIERRA_FORTRAN(pyr_gradient_operator)
         ( &nface,
           &nodesPerElement_,
